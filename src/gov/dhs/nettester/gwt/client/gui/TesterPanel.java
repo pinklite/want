4c7911c9a038e2d6376024f814f487554e1de5ab
U.S. Department of Homeland Security <37265803+dhs-admin@users.noreply.github.com>
2020-01-25 04:57:10 -0500

    Removed public domain banner.

--- src/gov/dhs/nettester/gwt/client/gui/TesterPanel.java
+++ src/gov/dhs/nettester/gwt/client/gui/TesterPanel.java
@@ -1,22 +1,3 @@
-/* This software was developed by employees of the Department of 
- * Homeland Security (DHS), an agency of the Federal Government.
- * Pursuant to title 15 United States Code Section 105, works of DHS
- * employees are not subject to copyright protection in the United States
- * and are considered to be in the public domain.  As a result, a formal
- * license is not needed to use the software.
- * 
- * This software is provided by DHS as a service and is expressly
- * provided "AS IS".  NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED
- * OR STATUTORY, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT
- * AND DATA ACCURACY.  NIST does not warrant or make any representations
- * regarding the use of the software or the results thereof including, but
- * not limited to, the correctness, accuracy, reliability or usefulness of
- * the software.
- * 
- * Permission to use this software is contingent upon your acceptance
- * of the terms of this agreement.
- */
 package gov.dhs.nettester.gwt.client.gui;
 
 import gov.dhs.nettester.gwt.client.GWTService;
@@ -63,10 +44,6 @@ import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.user.client.ui.Image;
 
-/**
- * 
- * @author steveq@nist.gov
- */
 public class TesterPanel extends DockLayoutPanel {
 
 	private static final String VERSION = "1.0a";
